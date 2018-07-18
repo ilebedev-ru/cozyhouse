887f8f70cfc943c63242b39f12901863e2c53df9
ilebedev-ru <wapstyle@bk.ru>
2018-07-18 21:18:56 +0500

    сделал копирование данных в модальное окно

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,23 @@
-var friendsButton = document.querySelectorAll(".friends__button");
+var friendsSlide = document.querySelectorAll('.friends__item');
 var friendsModal = document.querySelector(".friends-modal");
-var friendsModalButton = document.querySelector(".friends-modal__button");
+
+var friendsButton = document.querySelectorAll(".friends__button");
+var friendsModalButton = friendsModal.querySelector(".friends-modal__button");
 
 for (var i = 0; i < friendsButton.length; i++) {
-
 	friendsButton[i].addEventListener("click", function (evt) {
-	    evt.preventDefault();
+	  evt.preventDefault();
+	  var modalId = event.target.getAttribute('data-id');
 
-	    friendsModal.classList.add("friends-modal--open");
+	  var friendName = friendsSlide[modalId-1].querySelector('.friends__name').textContent
+	  friendsModal.querySelector('.friends-modal__name').textContent = friendName;
+
+	  var friendImgCollection = friendsSlide[modalId-1].getElementsByTagName('img');
+
+	  var friendImgModalCollection = friendsModal.getElementsByTagName('img');
+	  friendImgModalCollection[0].src = friendImgCollection[0].src
+
+	  friendsModal.classList.add("friends-modal--open");
 	});
 }
 
@@ -16,3 +26,5 @@ friendsModalButton.addEventListener("click", function (evt) {
 
     friendsModal.classList.remove("friends-modal--open");
 });
+
+///тут кароче надо скопировать контент из карточки и вставить в модальное окно!!
